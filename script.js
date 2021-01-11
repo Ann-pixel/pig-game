781e83e89c317c1d56a11a7293ed56f70158fd98
Gauri Mhaiskar <gauri.mhaiskar@gmail.com>
2021-01-11 09:03:35 -0500

    dice and hold logic

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-'use strict';
+"use strict";
+
+let dice;
+let currentScore1;
+let currentScore2;
+let isHolding = false;
+
+document.querySelector(".btn--roll").addEventListener("click", rollDice);
+document.querySelector(".btn--hold").addEventListener("click", function () {
+  isHolding = !isHolding;
+});
+
+function startGame() {
+  document.querySelector(".dice").style();
+}
+
+function rollDice() {
+  dice = Math.trunc(Math.random() * 6) + 1;
+  document.querySelector(".dice").setAttribute("src", `dice-${dice}.png`);
+}
+// rollDice();
