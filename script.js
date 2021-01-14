da5b320bb0dc41daa72de0d4d8698cb0387db244
Gauri Mhaiskar <gauri.mhaiskar@gmail.com>
2021-01-14 08:04:26 -0500

    changed up some logic

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 "use strict";
 
-let dice;
-let currentScore1;
-let currentScore2;
-let isHolding = false;
+//selecting elements
+//cumulative score
+const score0E = document.querySelector("#score--0");
+const score1E = document.querySelector("#score--1");
+//current score
+const current0E = document.getElementById("current--0");
+const current1E = document.getElementById("current--1");
 
-document.querySelector(".btn--roll").addEventListener("click", rollDice);
-document.querySelector(".btn--hold").addEventListener("click", function () {
-  isHolding = !isHolding;
+const diceE = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+let currentScore = 0;
+//starting conditions
+score0E.textContent = 0;
+score1E.textContent = 0;
+diceE.classList.add("hidden");
+
+//dice roll
+
+btnRoll.addEventListener("click", function () {
+  const randomNum = Math.trunc(Math.random() * 6) + 1;
+  diceE.classList.remove("hidden");
+  diceE.setAttribute("src", `dice-${randomNum}.png`);
+  if (randomNum !== 1) {
+    currentScore += randomNum;
+    current0E.textContent = currentScore;
+  } else {
+  }
 });
-
-function startGame() {
-  document.querySelector(".dice").style();
-}
-
-function rollDice() {
-  dice = Math.trunc(Math.random() * 6) + 1;
-  document.querySelector(".dice").setAttribute("src", `dice-${dice}.png`);
-}
-// rollDice();
